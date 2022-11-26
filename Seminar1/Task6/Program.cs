c2aa058ab335dbeb15975189aaf3f98c1e8d62d0
MuRoman <muroman@mail.ru>
2022-11-26 15:24:34 +0300

    add Seminar1

---
+++ Seminar1/Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.Write("Введите целое число: ");
+string input = Console.ReadLine();
+if (int.TryParse(input, out int number)) 
+{
+    if (number % 2 == 0) {Console.WriteLine("Число {0} четное", number);}
+    else {Console.WriteLine("Число {0} нечетное", number);}
+}
+else {Console.WriteLine("Ошибка ввода, попробуйте еще раз");}
---
+++ Seminar1/Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Write("Введите целое число N>0: ");
+if (!(int.TryParse(Console.ReadLine(), out int N)) | N <=0 ) {Console.WriteLine("Ошибка ввода, попробуйте еще раз"); Environment.Exit(0);}
+if (N == 1) {Console.WriteLine("От 1 до {0} четных чисел нет", N);}
+else {
+    Console.Write("Четные числа от 1 до {0}: ", N);
+    for (int i = 2; i < N-1; i += 2) {Console.Write("{0}, ", i);}
+    if (N % 2 == 1) {Console.WriteLine("{0}.", N-1);}
+    else {Console.WriteLine("{0}.", N);}
+}
